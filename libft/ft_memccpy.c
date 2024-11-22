10ca8f451be7d95a9d5cfdd42cf70ed383fe4d0a
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2024-11-22 11:31:29 +0100

    latest updates, added new projects subjects

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: nidruon <nidruon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 18:17:20 by nidruon           #+#    #+#             */
-/*   Updated: 2024/11/06 17:09:48 by nidruon          ###   ########.fr       */
+/*   Updated: 2024/11/20 11:53:29 by nidruon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,9 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		*(unsigned char *)dst = *(unsigned char *)src;
-		if (*(unsigned char *)src == (unsigned char)c)
-		{
-			dst++;
-			return (dst);
-		}
-		dst++;
-		src++;
+		*((unsigned char *)dst + i) = *((unsigned char *)src + i);
+		if (*((unsigned char *)src + i) == (unsigned char)c)
+			return ((unsigned char *)dst + i + 1);
 		i++;
 	}
 	return (NULL);
